75b0944d5f1c2e29dd0636c8f4e64ef4438870e9
unknown <luis.scobr@gmail.com>
2019-02-24 13:54:01 -0500

    200 response from mid server

--- raw_metric_cpu.ps1
+++ raw_metric_cpu.ps1
@@ -3,10 +3,6 @@ $text = Get-Content credentials.txt
 $user = $text[0]
 $pass = $text[1]
 
-#$text1 = Get-Content c:/CloudDimensions/EventGeneration/host2.txt
-$node = $text1[0]
-$hostname = $text1[1]
-#test
 # Build auth header
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $pass)))
 
@@ -20,20 +16,26 @@ $uri = $text[2]
 # Specify HTTP method
 $method = "post"
 
-# Specify request body
-$hash = @{
-		source = "SolarWinds";
-		node = "$node";
-		message_key = "MID10003";
-        severity    = "2";
-        description = "Low Disk Space on Server " + $hostname;
-		type = "MIDSimulator";
-		event_class = "hardware";
-		additional_info = "Status of server is up"
-         }
+#Get time in Unix Format
+$unix_time = [int][double]::Parse((Get-Date -UFormat %s))
 
+#Get random value for testing
+$value = Get-Random -Maximum 100
+
+# Specify request body
+$hash =@{
+            metric_type = 'Disk E: % Free Space'
+			resource = "E:\";
+			node = "nywvdpr02";
+			value  = $value;
+			timestamp = $unix_time;
+			source = "PRTG Metrics"
+        }
+				 
 # Convert hash to JSON
-$body = $hash | ConvertTo-Json
+$body = "[$($hash | ConvertTo-Json)]"
+
+Write-Host $body
 
 # Send HTTP request
 $response = Invoke-WebRequest -ContentType 'application/json' -Headers $headers -Method $method -Uri $uri -Body $body
