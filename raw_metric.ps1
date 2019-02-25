3155dc22b28171848859d4565dd1e11e3e1dbc9a
unknown <luis.scobr@gmail.com>
2019-02-25 11:56:30 -0500

    Version sent to HCA

--- raw_metric.ps1
+++ raw_metric.ps1
@@ -4,12 +4,25 @@
 # ----------------------------
 # 1.0      initial release
 # # # # # # # # # # # # # # # # # # # # # # # # # #
+<# Parameters required are the follwing:
+metric_type	Name of the metric. e.g. "Disk C: % Free Space/""
+resource	Information about the resource for which metric data is being collected. In the example below, C:\ is the resource for which metric data is collected. e.g. "C:\\"
+node	IP, FQDN, name of the CI, or host. In the example below, the name of the Linux server where the disks are installed. e.g. "myserver1.domain.com"
+value	Value of the metric.  e.g. 50
+timestamp	Epoch timestamp of the metric in milliseconds.  e.g. "1473183012000"
+source	Data source monitoring the metric type. e.g. "PRTG Metrics"
+#>
+
+
+#Pass placeholders from PRTG custom notification that match the variables below with no value assigned
 param( 
-       [string]$sensor       = "",    #metric_name
-       [string]$name         = "",    #resource 
-       [string]$hostname     = "",    #node
-          [int]$lastvalue    = ""     #value
-     #[DateTime]$timestamp = ""      #timestamp
+		[string]$sensor,    #metric_name
+		[string]$name,      #resource 
+		[string]$hostname,  #node
+		  [int]$lastvalue,  #value
+		[string]$source     = "PRTG Metrics",    #source
+	    [int64]$timestamp   = [int][double]::Parse((Get-Date (get-date).touniversaltime() -UFormat %s)) * 1000   #timestamp
+	   
 )
 
 # Eg. User name="admin", Password="admin" for this code sample.
@@ -24,17 +37,10 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add('Authorization',('Basic {0}' -f $base64AuthInfo))
 
 # Specify endpoint uri - REST Protocol
-$uri = "http://192.168.22.25:8001/api/mid/sa/metrics"
+$uri = "http://10.96.162.38:8001/api/mid/sa/metrics"
 
 # Specify HTTP method
 $method = "post"
-
-#Get time in Unix Format
-$mills_time = [int][double]::Parse((Get-Date (get-date).touniversaltime() -UFormat %s)) * 1000
-
-
-#Get random value for testing
-$value = Get-Random -Minimum 40 -Maximum 50
 
 # Specify request body
 $hash =@{
@@ -42,8 +48,8 @@ $hash =@{
 			resource = $name;
 			node = $hostname;
 			value  = $lastvalue;
-			timestamp = $mills_time;
-			source = "PRTG Metrics"
+			timestamp = $timestamp;
+			source = $source
 
         }
 
